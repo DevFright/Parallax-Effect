2bda41feb76e5575903947c5b358bb6c54335964
Matthew Newill <matt@gvenue.com>
2014-09-05 13:17:40 +0100

    Parallax effect added to view.
    Added code in to make the background move when device moved. Must be
    used on an iOS device rather than the simulator.

--- Parallax Effect/DFViewController.h
+++ Parallax Effect/DFViewController.h
@@ -2,12 +2,14 @@
 //  DFViewController.h
 //  Parallax Effect
 //
-//  Created by Matthew Newill on 05/09/2014.
-//  Copyright (c) 2014 TechAsis Limited. All rights reserved.
+//  Created by Martin Bateson on 05/09/2014.
+//  Copyright (c) 2014 DevFright.com. All rights reserved.
 //
 
 #import <UIKit/UIKit.h>
 
 @interface DFViewController : UIViewController
+
+@property (weak, nonatomic) IBOutlet UIImageView *backGroundView;
 
 @end
--- Parallax Effect/DFViewController.m
+++ Parallax Effect/DFViewController.m
@@ -2,8 +2,8 @@
 //  DFViewController.m
 //  Parallax Effect
 //
-//  Created by Matthew Newill on 05/09/2014.
-//  Copyright (c) 2014 TechAsis Limited. All rights reserved.
+//  Created by Martin Bateson on 05/09/2014.
+//  Copyright (c) 2014 DevFright.com. All rights reserved.
 //
 
 #import "DFViewController.h"
@@ -14,10 +14,38 @@
 
 @implementation DFViewController
 
+- (void)viewWillAppear:(BOOL)animated {
+    [self backGroundMotion];
+}
+
 - (void)viewDidLoad
 {
     [super viewDidLoad];
 	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)backGroundMotion {
+    CGFloat min = -20.0f;
+    CGFloat max = 20.0f;
+    
+    // create the x axis motion
+    UIInterpolatingMotionEffect *xAxis = [[UIInterpolatingMotionEffect alloc]initWithKeyPath:@"center.x" type:UIInterpolatingMotionEffectTypeTiltAlongHorizontalAxis];
+    
+    xAxis.minimumRelativeValue = @(min);
+    xAxis.maximumRelativeValue = @(max);
+    
+    // create the y axis motion
+    UIInterpolatingMotionEffect *yAxis = [[UIInterpolatingMotionEffect alloc]initWithKeyPath:@"center.y" type:UIInterpolatingMotionEffectTypeTiltAlongVerticalAxis];
+    
+    yAxis.minimumRelativeValue = @(min);
+    yAxis.maximumRelativeValue = @(max);
+    
+    // combine these is a group
+    UIMotionEffectGroup *xyGroup = [[UIMotionEffectGroup alloc]init];
+    xyGroup.motionEffects = @[xAxis, yAxis];
+    
+    //  apply what was created to the image
+    [self.backGroundView addMotionEffect:xyGroup];
 }
 
 - (void)didReceiveMemoryWarning
